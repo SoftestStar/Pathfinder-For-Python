cd512ed341a5d5ce688bbb187ca9a70ca17726ce
SoftestStar <142770819+SoftestStar@users.noreply.github.com>
2026-09-19 23:44:18 +0700

    BFS are done now
    Now BFS are working. Though animation still don't so look good, I will fix later.
    [node class]
    add set/check color
    add get pos
    
    [general algorithm]
    add BFS
    add finding neighbor
    add make path
    
    add created grid
    add draw gird
    add find clicked pos

--- Node.py
+++ Node.py
@@ -1,9 +1,69 @@
 import pygame
 
-class node:
-    def __init__(self):
+WHITE = (255,255,255)
+BLACK = (000,000,000)
+RED = (255,000,000)
+GREEN = (000,255,000)
+BLUE = (000,000,255)
+YELLOW = (255,255,000)
+PURPLE = (128, 0, 128)
+
+class create_node:
+    def __init__(self, row, col, width, all_rows):
+        self.row = row
+        self.col = col
+
+        self.x = row * width
+        self.y = col * width
+
+        self.width = width
+        self.all_rows = all_rows
+
+        self.color = WHITE
+        self.neighbor = []
+    
+    def get_position(self):
+        return self.row, self.col
+
+    def draw(self, screen):
+        #draw a cube in screen
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.width))
+
+    def update_neighbors(self, grid):
         pass
-    def draw(self):
+
+    #chack color
+    def is_empty(self):
+        return self.color == WHITE
+    def is_wall(self):
+        return self.color == BLACK
+    def is_start(self):
+        return self.color == YELLOW
+    def is_end(self):
+        return self.color == PURPLE
+    def is_path(self):
+        return self.color == BLUE
+    def is_currenly_visit(self):
+        return self.color == GREEN
+    def is_visited(self):
+        return self.color == RED
+
+    #Set color
+    def set_visited(self):
+        self.color = RED
+    def set_empty(self):
+        self.color = WHITE
+    def set_wall(self):
+        self.color = BLACK
+    def set_start(self):
+        self.color = YELLOW
+    def set_end(self):
+        self.color = PURPLE
+    def set_path(self):
+        self.color = BLUE
+    def set_currenly_visit(self):
+        self.color = GREEN
+
+    def __lt__(self, other_node): # function for compare Node
         pass
-    def __lt__(self, other): # function for compare Node
-        pass
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import general_algorithm as gal
+import time
 
 ROW = 50
 WIDTH = 800
@@ -13,27 +14,64 @@ BLACK = (000,000,000)
 
 clock = pygame.time.Clock()
 
+grid = gal.create_grid(ROW,WIDTH)
+
+start_pos = None
+end_pos = None
+
+start_finding = False
+
 is_running = True
-
-def mouse_act():
-    if pygame.mouse.get_pressed()[0]:
-        print(pygame.mouse.get_pos())
-    elif pygame.mouse.get_pressed()[2]:
-        pass
-
 while is_running:
+
+    #Get row col
+    row, col = gal.find_clicked_position(pygame.mouse.get_pos(),ROW,WIDTH)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
 
-    screen.fill(BLACK)
+        if pygame.mouse.get_pressed()[0]:
 
-    gal.draw_grid(screen,WIDTH,ROW,WHILE)
+            # check if it out of grid
+            if len(grid) - 1 < row or len(grid[row]) - 1 < col or row < 0 or col < 0:
+                continue
 
-    mouse_act()
+            # draw
+            if not start_pos and not grid[row][col].is_wall():
+                grid[row][col].set_start()
+                start_pos = grid[row][col]
+            elif not end_pos and not grid[row][col].is_wall() and not grid[row][col].is_start():
+                grid[row][col].set_end()
+                end_pos = grid[row][col]
+            elif not grid[row][col].is_start() and not grid[row][col].is_end():
+                grid[row][col].set_wall()
+        
+        #set empty
+        elif pygame.mouse.get_pressed()[2]:
+            if grid[row][col].is_start():
+                start_pos = None
+            elif grid[row][col].is_end():
+                end_pos = None
+            grid[row][col].set_empty()
 
-    gal.created_grid()
+        if event.type == pygame.KEYDOWN:
+            #claer all
+            if event.key == pygame.K_c:
+                for row in grid:
+                    for node in row:
+                        node.set_empty()
+                        start_pos = None
+                        end_pos = None
+            #start finding
+            if event.key == pygame.K_SPACE:
+               if start_pos and end_pos:
+                gal.Bfs_alogorithm(screen, grid,start_pos,end_pos,ROW,WIDTH)
+
+    screen.fill(WHILE)
+
+    gal.draw_node(screen,grid)
+    gal.draw_grid(screen,ROW,WIDTH)
 
     pygame.display.flip()
     pygame.display.set_caption(f"Pathfinder for Python ({clock.get_fps():.2f})")
