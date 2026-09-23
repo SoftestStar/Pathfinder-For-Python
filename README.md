487a25895768f7096677459b322196c5ff72e09b
SoftestStar <142770819+SoftestStar@users.noreply.github.com>
2026-09-24 00:04:46 +0700

    create gitignore, make code more easy to read, fix Bfs function and how to use in readme.md
    create .gitignore
    
    [main.py]
    - make function for key to make it easy to read in while loop
    
    [Bfs function]
    - replace pygame.time.wait to pygame.time.get_ticks()
    
    [general_algorithm.py]
    - create update_grid
    
    [README.md]
    - add how to Bfs function
    - add what key can do

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Pathfinder-For-Python
+
+**Pathfinder** is an algorithm that finds the shortest path using a graph.
+
+# Library
+
+You may need a **pygame** library to run this project.
+You can use this in your **terminal** to install the **pygame**: 
+
+` pip install pygame `
+
+# Button
+
+### Keybord
+- `c` : clear all
+- `space` : start
+
+### Mouse
+
+- `first left click` : mark as start point
+- `seccon left click` : mark as end point
+- `other left click` : mark as wall point
+
+- `right click` : mark as empty
+
+# BFS
+
+You can call ` Bfs_alogorithm() ` from ` general_algorithm.py `, it had 8 parameters:
+
+- **screen**
+    - A **pygame screen object** used to disply the process.
+- **grid**
+    - A grid that made by ` create_grid() ` function in ` general_algorithm.py `.
+- **start**
+    - A **starting node** of the path.
+- **end**
+    - An **destination node** of the path.
+- **rows**
+    - **Totol rows** of the grid.
+- **width**
+    - The width of your screen
+- **step_by_step**
+    - If set to ` True `, the process is displayed **step by step**. If set to ` False `, the entrie process is displayed **all at once**. The **Default** is ` False `
+- **delay**
+    - Controls how long the screen update is delayed, measured in **milliseconds**. The **Default** is ` 1 `
--- general_algorithm.py
+++ general_algorithm.py
@@ -2,6 +2,24 @@ import pygame
 import Node
 from collections import deque
 
+WHITE = (255,255,255)
+BLACK = (000,000,000)
+RED = (255,000,000)
+GREEN = (000,255,000)
+BLUE = (000,000,255)
+YELLOW = (255,255,000)
+PURPLE = (128, 0, 128)
+
+
+#Pygame
+
+def update_grid(screen,grid,rows,width):
+
+    screen.fill(WHITE)
+    draw_node(screen, grid)
+    draw_grid(screen, rows, width)
+    pygame.display.flip()
+
 #Finding algorithm
 
 def find_neighbor(grid, node):
@@ -15,40 +33,56 @@ def find_neighbor(grid, node):
     for dr, dc in directions:
         new_row, new_col = row + dr, col + dc
         if 0 <= new_row < rows and 0 <= new_col < cols:
-            if grid[new_row][new_col].is_empty() or grid[new_row][new_col].is_end():
+            if grid[new_row][new_col].is_empty() or grid[new_row][new_col].is_end() or grid[new_row][new_col].is_currenly_visit():
                 if not grid[new_row][new_col].is_end():
                     grid[new_row][new_col].set_currenly_visit()
                 neighbors.append(grid[new_row][new_col])
     
     return neighbors
 
-def Bfs_alogorithm(screen ,grid, start ,end, rows, width):
+def make_path(path):
+    for i in path:
+        if not i.is_start() and not i.is_end():
+            i.set_path()
+
+# BFS
+def Bfs_alogorithm(screen ,grid, start ,end, rows, width, step_by_step = False, delay = 1):
 
     visited = {start: None}
     queue = deque([start])
     meet_end = False
 
+    start_time = pygame.time.get_ticks()
+
     while queue and not meet_end:
+
+        end_time = pygame.time.get_ticks()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return None
 
-        node = queue.popleft()
-        if node == end:
-            meet_end = True
-        for neighbor in find_neighbor(grid, node):
-            if neighbor not in visited:
-                if not neighbor.is_end():
-                    neighbor.set_visited()
-                visited[neighbor] = node
-                queue.append(neighbor)
+        if end_time - start_time >= delay:
+            start_time = end_time
 
-        screen.fill((255, 255, 255))
-        draw_node(screen, grid)
-        draw_grid(screen, rows, width)
-        pygame.display.flip()
-        pygame.time.wait(1)
+            if not step_by_step:
+                for i in queue: # set all neighbor for butter animation
+                    find_neighbor(grid, i)
+                update_grid(screen,grid,rows,width)
+
+            node = queue.popleft()
+            if node == end:
+                meet_end = True
+            
+            for neighbor in find_neighbor(grid, node):
+                if step_by_step:
+                    update_grid(screen,grid,rows,width)
+                if neighbor not in visited:
+                    if not neighbor.is_end():
+                        neighbor.set_visited()
+                    visited[neighbor] = node
+                    queue.append(neighbor)
     
     if end not in visited:
         return None
@@ -63,10 +97,6 @@ def Bfs_alogorithm(screen ,grid, start ,end, rows, width):
     path.reverse()
     make_path(path)
 
-def make_path(path):
-    for i in path:
-        if not i.is_start() and not i.is_end():
-            i.set_path()
 
 #Grid
 
